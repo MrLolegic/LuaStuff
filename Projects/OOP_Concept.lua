6290b86deb7771ab01ba2e01e5abdfc9650ee782
Mr Lolegic <90881896+MrLolegic@users.noreply.github.com>
2023-02-08 08:49:23 -0600

    Update OOP_Concept.lua

--- Projects/OOP_Concept.lua
+++ Projects/OOP_Concept.lua
@@ -1,35 +1,76 @@
 --// Made by Mr. Lolegic \\--
 --// Not much of a concept just showing how I think roblox works, also showing some OOP cuz why not.
-
---// Game class definition
+-- Game class definition
 local Game = {}
 Game.__index = Game
 
 function Game.new()
     local self = setmetatable({}, Game)
-
     self.Players = {
         LocalPlayer = {
-            AccountAge = 10
+            AccountAge = 10,
+            Name = "Player 1",
+            Character = {
+                Humanoid = {
+                    Health = 100
+                }
+            }
         }
     }
-
+    self.Services = {
+        Players = self.Players,
+    }
     return self
 end
 
 function Game:GetService(Service)
-    if Service == "Players" then
-        return self.Players
-    end
+    return self.Services[Service]
 end
 
---// Creating an instance of the Game class
-_G.game = Game.new()
+function Game:AddPlayer(name)
+    table.insert(self.Players, {Name = name, AccountAge = 0, Character = {Humanoid = {Health = 100}}})
+    print("Player " .. name .. " has joined the game!")
+end
 
---// Testing
+function Game:GetPlayerCount()
+    return #self.Players
+end
+
+function Game:FindFirstChild(childName)
+    for i, player in ipairs(self.Players) do
+        if player.Name == childName then
+            return player
+        end
+    end
+
+    return nil
+end
+
+function Game:WaitForChild(childName)
+    repeat
+        local child = self:FindFirstChild(childName)
+        if child then
+            return child
+        end
+    until false
+end
+
+-- Creating an instance of the Game class
+local game = Game.new()
+
+-- Testing
 print("Regular way Example: " .. game.Players.LocalPlayer.AccountAge)
 print("GetService Example: " .. game:GetService("Players").LocalPlayer.AccountAge)
 
---// Using getfenv()
-print("Getfenv Example: " .. getfenv()["game"]["Players"]["LocalPlayer"]["AccountAge"])
-print("Getfenv - GetService, Example: " .. getfenv()["game"]["GetService"](getfenv()["game"], "Players")["LocalPlayer"]["AccountAge"])
+game:AddPlayer("Player 2")
+print("Number of players: " .. game:GetPlayerCount())
+
+local player = game:FindFirstChild("Player 2")
+if player then
+    print("Found player: " .. player.Name .. " with health " .. player.Character.Humanoid.Health)
+else
+    print("Player not found")
+end
+
+local player = game:WaitForChild("Player 2")
+print("Found player: " .. player.Name .. " with health " .. player.Character.Humanoid.Health)
